9ca62a6f0cfc4b040e04bf59d39dec8dc2f01624
kleuvyn <kleuvyn@hotmail.com>
2023-11-28 00:54:24 -0300

    css @media screen

--- script.js
+++ script.js
@@ -13,6 +13,21 @@ function esconderTodasCategorias() {
     });
 }
 
+function redirecionar(url) {
+  location.href = url;
+}
+
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open');
+      document.querySelector('.icon').src="https://img.icons8.com/?size=256&id=quuyJoZCkYni&format=png";
+  } else {
+      menuMobile.classList.add('open');
+      document.querySelector('.icon').src="https://img.icons8.com/?size=256&id=K7OXfoF0zHXw&format=pngg";
+  }
+}
+
 function inicio() {
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
     const totalValor = document.getElementById("total-valor");
@@ -43,25 +58,27 @@ function inicio() {
       }
       
       const btn = document.querySelector('.modalBtn')
-      btn.addEventListener('click', switchModal)
+      if (btn) {
+        btn.addEventListener('click', switchModal)
       
-      window.onclick = function(event) {
-          const modal = document.querySelector('.modal')
-        if (event.target == modal) {
-          switchModal()
+        window.onclick = function(event) {
+            const modal = document.querySelector('.modal')
+          if (event.target == modal) {
+            switchModal()
+          }
         }
       }
     
     const confirmarPedidoButton = document.getElementById("confirmar-pedido");
-    confirmarPedidoButton.addEventListener("click", function() {
+    if (confirmarPedidoButton) {
+      confirmarPedidoButton.addEventListener("click", function() {
 
         const paragrafo = document.getElementById('texto-modal');
 
         paragrafo.innerHTML = `Pedido confirmado!<br>Valor: ${totalValor.textContent}.<br>Número do pedido: ${Math.trunc(Math.random()*1000)}.`;
         switchModal(`Pedido confirmado!\nValor: ${totalValor.textContent}.\nNúmero do pedido: ${Math.trunc(Math.random()*1000)}.`);
-    
-        
-    });
+      });
+    }
     
     esconderTodasCategorias();
 }
